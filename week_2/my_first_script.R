08c9adfe4aa2ebfdd43b71fcf6880c70f2df48f5
Ajul Thomas <ajul.thomas@outlook.com>
2023-08-15 11:57:18 +1000

    Initial commit

---
+++ week_2/my_first_script.R
@@ -0,0 +1,15 @@
+# Get library locations that contain R packages 
+.libPaths()
+
+# Get the list of all the packages installed
+library()
+
+# Get all packages currently loaded in the R environment
+search()
+
+# Install new package called "XML"
+install.packages("XML")
+
+# Load the package
+library("XML")
+
